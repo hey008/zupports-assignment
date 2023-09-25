0e11eeb59d90d347280250110a1c7c311a5c1436
Jane Maverick <hey008@gmail.com>
2023-09-25 16:47:01 +0700

    Add Vue-Router, Implement Layout Structure

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand">Zupports Assignment</a>
+        </div>
+    </nav>
+
+    <div class="main">
+        <router-view />
+    </div>
+
+    <div class="bg-dark sticky-bottom p-2">
+        <div class="small text-white ms-auto">Code by Jirawit Inkhao</div>
+    </div>
+
+</template>
---
+++ resources/js/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>INDEX PAGE</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Index mounted.')
+        }
+    }
+</script>
