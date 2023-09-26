b3d035ccacf85b8ba2d505d5512e511ba8483676
Jane Maverick <hey008@gmail.com>
2023-09-27 00:44:11 +0700

    Add GoogleMap feature

--- resources/js/pages/IndexPage.vue
+++ resources/js/pages/IndexPage.vue
@@ -12,18 +12,42 @@
                         <h5 class="card-title">{{ result.name }}</h5>
                         <p class="card-text">{{ result.formatted_address }}</p>
                     </div>
+                    <div class="card-footer d-grid gap-2">
+                        <button @click="openMap(result)" class="btn btn-primary btn-block">Map</button>
+                    </div>
                 </div>
             </div>
         </div>
         <div v-show="errorDisplay" class="alert alert-danger" role="alert">Sorry! Something went wrong</div>
     </div>
+
+    <div class="modal fade" :class="{ show:isModalOpen }">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalToggleLabel">{{ modalTitle }}</h1>
+                    <button class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="closeMap()"></button>
+                </div>
+                <div class="modal-body p-0">
+                    <GoogleMap api-key="AIzaSyBOivj6tthdx0Kx6UJgtSbn5PXhH_LhHSo" style="width: 100%; height: 100%" :center="mapPoint" :zoom="19">
+                        <Marker :options="{ position: mapPoint }" />
+                    </GoogleMap>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
 import { ref, watch } from 'vue';
 import { useStore, mapGetters } from 'vuex';
+import { GoogleMap, Marker } from "vue3-google-map";
 
 export default {
+    components: {
+        GoogleMap, Marker
+    },
     computed: {
         ...mapGetters(["textSearch"])
     },
@@ -44,7 +68,11 @@ export default {
         let loading = ref(false);
         let errorDisplay = ref(false);
         let results = ref([]);
+        let isModalOpen = ref(false);
+        let modalTitle = ref("");
         const store = useStore();
+
+        let mapPoint = ref({ lat: 40.689247, lng: -74.044502 });
 
         function construct () {
             fetchData(store.state.textSearch);
@@ -83,10 +111,22 @@ export default {
             results.value = jsonResponse.data.results;
         }
 
+        function openMap(mapData) {
+            console.log(mapData);
+            modalTitle.value = mapData.name;
+            mapPoint.value = { lat: mapData.geometry.location.lat, lng: mapData.geometry.location.lng };
+            isModalOpen.value = true;
+        }
+
+        function closeMap() {
+            isModalOpen.value = false;
+        }
+
         return {
             construct, fetchData,
             loading, openLoading, closeLoading, errorDisplay,
-            render, results
+            render, results,
+            isModalOpen, openMap, closeMap, mapPoint, modalTitle
         }
     }
 }
